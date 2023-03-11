6d67f04a682fbb411e89a453e2cef153136ef4c8
wndks <sesangcham0520@gmail.com>
2023-03-11 13:50:40 +0900

    create pages + routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
 
 function App() {
   return (
-    <div> 
-      <h1 className='text-5xl' > Hello World</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path ="/" element={<Home/>} /> 
+      </Routes>
+
+    </Router>
   );
 }
 
